c4d099d536b2d382584ae4e662fdbd8eb90279b7
Lulu Sheng <lulu.sheng@shopify.com>
2018-02-12 17:41:29 -0500

    lulz

---
+++ html/canvas.js
@@ -0,0 +1,245 @@
+// Lulu Sheng
+// 101072946
+// Assignment 1
+// COMP2406
+// Louis Nel
+// 2018-02-01
+
+var canvas = document.getElementById('canvas1');
+
+var wordsDrawn = [];
+var file = [];
+
+function getWordAtLocation(aCanvasX, aCanvasY){
+    var context = canvas.getContext('2d');
+
+    for(var i=0; i<wordsDrawn.length; i++){
+        var wordLength = context.measureText(wordsDrawn[i].word).width;
+
+        // Collision detection between mouse press and word
+        if((aCanvasX - wordsDrawn[i].x < wordLength) && 
+            (aCanvasX - wordsDrawn[i].x > 0) &&
+            (wordsDrawn[i].y - aCanvasY < 20) && 
+            (wordsDrawn[i].y - aCanvasY > 0)) {
+            return wordsDrawn[i];
+        }
+    }
+    return null;
+}
+
+var drawCanvas = function() {
+    var context = canvas.getContext('2d');
+
+    context.fillStyle = 'white';
+    context.fillRect(0,0,canvas.width,canvas.height); //erase canvas
+
+    context.font = '20pt Arial';
+    context.fillStyle = 'cornflowerblue';
+    context.strokeStyle = 'blue';
+
+    // draws quote
+    for(var i=0; i<wordsDrawn.length; i++){ 
+        var data = wordsDrawn[i];
+        context.fillText(data.word, data.x, data.y);
+        context.strokeText(data.word, data.x, data.y);
+    }
+}
+
+function handleMouseDown(e){
+
+    //get mouse location
+    var rect = canvas.getBoundingClientRect();
+
+    //use jQuery event object pageX and pageY
+    var canvasX = e.pageX - rect.left; 
+    var canvasY = e.pageY - rect.top;
+
+    wordBeingMoved = getWordAtLocation(canvasX, canvasY);
+
+    if(wordBeingMoved != null ){
+        deltaX = wordBeingMoved.x - canvasX; 
+        deltaY = wordBeingMoved.y - canvasY;
+        $("#canvas1").mousemove(handleMouseMove);
+        $("#canvas1").mouseup(handleMouseUp);
+
+    }
+
+    // Stop propagation of the event and stop any default 
+    //  browser action
+    e.stopPropagation();
+    e.preventDefault();
+
+    // update the canvas
+    drawCanvas();
+}
+
+function handleMouseMove(e){
+
+    console.log("mouse move");
+
+    //get mouse location 
+    var rect = canvas.getBoundingClientRect();
+    var canvasX = e.pageX - rect.left;
+    var canvasY = e.pageY - rect.top;
+
+    // update the wordBeingMoved position
+    wordBeingMoved.x = canvasX + deltaX;
+    wordBeingMoved.y = canvasY + deltaY;
+
+    e.stopPropagation();
+
+    drawCanvas();
+}
+
+function handleMouseUp(e){
+    console.log("mouse up");
+
+    e.stopPropagation();
+
+    //remove mouse move and mouse up handlers but 
+    //leave mouse down handler
+    $("#canvas1").off("mousemove", handleMouseMove); 
+    $("#canvas1").off("mouseup", handleMouseUp); 
+
+    drawCanvas(); 
+}
+
+
+function handleSubmitButton() {
+    var context = canvas.getContext('2d');
+
+    // Get text from user input field
+    var userText = $('#userTextField').val();
+
+    if(userText && userText != '') {
+
+        var userRequestObj = {text: userText}; 
+        var userRequestJSON = JSON.stringify(userRequestObj);
+        $('#userTextField').val(''); //clear the user text field
+
+        $.post("userText", userRequestJSON, function(data, status) {
+            console.log("data: " + data);
+            console.log("typeof: " + typeof data);
+            var responseObj = JSON.parse(data);
+
+            if (responseObj.lineArray) {
+
+                // Create objects representing words and their locations
+                var yPosition=50;
+                var spacingBetweenWords = 20;
+                var words = [];
+
+                for (var i = 0; i < responseObj.lineArray.length; i++) {
+                    var arrayOfWords = responseObj.lineArray[i].split(" ");
+                    var xPosition = 50;
+                    for (var j = 0; j < arrayOfWords.length; j++) {
+                        words.push({word: arrayOfWords[j], x: xPosition, y:yPosition});
+                        xPosition += context.measureText(arrayOfWords[j]).width + spacingBetweenWords;
+                    }
+                    yPosition += 60;
+                }
+
+                // R3.3 The lyrics and chords shown on canvas are in chord-pro format
+                wordsDrawn = words;
+                drawCanvas();
+
+                // R3.4 The chord pro text downloaded from the server is shown
+                // as paragraph lines below the canvas
+                let textDiv = document.getElementById("text-area");
+                let textParagraph = "";
+                for (var i = 0; i < responseObj.lineArray.length; i++) {
+                    textParagraph = textParagraph + `<p> ${responseObj.lineArray[i]} </p>`;
+                }
+                textDiv.innerHTML = textParagraph;
+
+            } else {
+                // R3.5 If the requested song does not exist, then the canvas appears blank
+                // and no there is no paragraph content below the canvas
+                wordsDrawn = [];
+                drawCanvas();
+                document.getElementById("text-area").innerHTML = ``;
+            }
+        });
+    }
+}
+
+function handleRefreshButton () {
+    let yPos = 50;
+    const lineOffset = 60;
+    let finalLines = [];
+
+    while (yPos <= $("#canvas1").height()) {
+        var line = [];
+
+        for (var i = 0; i < wordsDrawn.length; i++) {
+            if (wordsDrawn[i].y > yPos - 50 && wordsDrawn[i].y < yPos + 10) {
+                line.push(wordsDrawn[i]);
+            }
+        }
+
+        line.sort(function (a, b) {
+            return a.x - b.x;
+        });
+
+        let tempLine = "";
+        for (var j = 0; j < line.length; j++) {
+            tempLine += line[j].word + " ";
+        }
+
+        console.log(tempLine);
+
+        finalLines.push(tempLine);
+        yPos += lineOffset;
+    }
+
+    let textDiv = document.getElementById("text-area");
+    let textParagraph = "";
+    for (var i = 0; i < finalLines.length; i++) {
+        textParagraph = textParagraph + `<p> ${finalLines[i]} </p>`;
+    }
+    textDiv.innerHTML = textParagraph;
+
+    file = finalLines;
+}
+
+function handleSaveAsButton() {
+    handleRefreshButton();
+
+    var result = "";
+    var userText = $('#userTextField').val();
+
+    for (var i = 0; i < file.length; i++) {
+        result += file[i] + "\n";
+    }
+
+    console.log(result);
+        
+    var userRequestObj = {fileTitle: userText, fileText: result}; 
+    var userRequestJSON = JSON.stringify(userRequestObj);
+    $('#userTextField').val(''); //clear the user text field
+
+    // CALLBACK FUNCTION WHAT
+    $.post("newFile", userRequestJSON, function(data, status) {
+        console.log("success");
+    });
+
+}
+
+var ENTER = 13;
+
+function handleKeyUp(e){
+    if(e.which == ENTER){		  
+        handleSubmitButton(); //treat ENTER key like you would a submit
+        $('#userTextField').val(''); //clear the user text field
+    }
+
+    e.stopPropagation();
+    e.preventDefault();
+}
+
+$(document).ready(function() {
+    $(document).keyup(handleKeyUp);
+    $("#canvas1").mousedown(handleMouseDown);
+
+    drawCanvas();
+});
---
+++ server.js
@@ -0,0 +1,135 @@
+// Lulu Sheng
+// 101072946
+// Assignment 1
+// COMP2406
+// Louis Nel
+// 2018-02-01
+
+var http = require("http"); //need to http
+var fs = require("fs"); //need to read static files
+var url = require("url"); //to parse url strings
+
+var ROOT_DIR = "html";
+var songs = ["Brown Eyed Girl", "Sister Golden Hair", "Peaceful Easy Feeling"];
+
+var MIME_TYPES = {
+    css: "text/css",
+    gif: "image/gif",
+    htm: "text/html",
+    html: "text/html",
+    ico: "image/x-icon",
+    jpeg: "image/jpeg",
+    jpg: "image/jpeg",
+    js: "text/javascript", //should really be application/javascript
+    json: "application/json",
+    png: "image/png",
+    txt: "text/plain"
+};
+
+var get_mime = function(filename) {
+    var ext, type;
+    for (ext in MIME_TYPES) {
+        type = MIME_TYPES[ext];
+        if (filename.indexOf(ext, filename.length - ext.length) !== -1) {
+            return type;
+        }
+    }
+    return MIME_TYPES["txt"];
+};
+
+http.createServer(function(request, response) {
+    var urlObj = url.parse(request.url, true, false);
+    console.log("\n============================");
+    console.log("PATHNAME: " + urlObj.pathname);
+    console.log("REQUEST: " + ROOT_DIR + urlObj.pathname);
+    console.log("METHOD: " + request.method);
+
+    var receivedData = "";
+
+    request.on("data", function(chunk) {
+        receivedData += chunk;
+    });
+
+    request.on("end", function() {
+        console.log("received data: ", receivedData);
+        console.log("type: ", typeof receivedData);
+
+        if (request.method == "POST") {
+            var dataObj = JSON.parse(receivedData);
+            console.log("received data object: ", dataObj);
+            console.log("type: ", typeof dataObj);
+
+            console.log("USER REQUEST: " + dataObj.text);
+            var returnObj = {};
+
+            if (dataObj.hasOwnProperty('fileTitle')) {
+                // CALLBACK WAHT
+                fs.writeFile('html/'+dataObj.fileTitle+'.txt', dataObj.fileText, (err) => {
+                    if (err) throw err;
+                });
+
+                songs.push(dataObj.fileTitle);
+
+            } else {
+                if (songs.includes(dataObj.text)) {
+                /*
+                if (dataObj.text == "Brown Eyed Girl" ||
+                    dataObj.text == "Peaceful Easy Feeling" || 
+                    dataObj.text == "Sister Golden Hair") {
+                    */
+                    var filePath = ROOT_DIR + "/" + dataObj.text +".txt";
+
+                    var lines = [];
+                    fs. readFile(filePath, function(err, data) {
+                        if (err) {
+                            console.log("ERROR: " + JSON.stringify(err));
+                            response.writeHead(404);
+                            response.end(JSON.stringify(err));
+                            return;
+                        }
+
+                        lines = data.toString().split("\n");
+                        returnObj.lineArray = lines;
+
+                        response.writeHead(200, { "Content-Type": get_mime(filePath) });
+                        response.end(JSON.stringify(returnObj));
+                    });
+                } else {
+                    // if the requested song does not exist, return
+                    // an empty object
+                    response.writeHead(200);
+                    response.end(JSON.stringify(returnObj));
+                }
+            }
+        }
+
+        if (request.method == "GET") {
+            var filePath = ROOT_DIR + urlObj.pathname;
+
+            fs.readFile(filePath, function(err, data) {
+                if (err) {
+                    console.log("ERROR: " + JSON.stringify(err));
+                    response.writeHead(404);
+                    response.end(JSON.stringify(err));
+                    return;
+                }
+
+                response.writeHead(200, { "Content-Type": get_mime(filePath) });
+                response.end(data);
+            });
+        }
+    });
+}).listen(3000);
+
+    console.log("Server Running CNTL-C to quit");
+
+
+
+
+
+
+
+
+
+
+
